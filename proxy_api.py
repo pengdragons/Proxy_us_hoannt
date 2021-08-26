33ba88ba49ca323b56d7ee61970389c40c4753c1
pengdragons <47887697+pengdragons@users.noreply.github.com>
2021-08-26 13:32:20 +0700

    Add files via upload

---
+++ proxy_api.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import request
+import json
+import os
+from threading import Thread
+
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+   return "Hello World!"
+@app.route("/proxy/<number_gen>",methods=["GET"])
+def excute_data(number_gen):
+    os.system('killall proxybroker')
+    mydata =[]
+    f = open("port.txt", "r")
+    PORT = f.read()
+
+    for i in range(int(number_gen)):
+        mydata.append({
+            'ip' : '103.75.184.246',
+            'port': PORT
+            })
+        PORT = int(PORT) + 1
+        print(PORT)
+    jsonstring = json.dumps(mydata)
+    f = open("port.txt", "w")
+    f.write(str(PORT))
+    f.close()
+    def do_work(number_gen):
+        os.system('python3 run_cmd.py '+number_gen)
+    thread = Thread(target=do_work, args=(number_gen,))
+    thread.start()
+    return(jsonstring)
+if __name__ == "__main__":
+   app.run(host='103.75.184.246')
---
+++ run_cmd.py
@@ -0,0 +1,43 @@
+
+import threading
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print ("Usage: python %s <number_proxy>" % sys.argv[0])
+    print ("Example: python %s 10" % sys.argv[0])
+    raise SystemExit
+
+try:
+    number_proxy = int(sys.argv[1])
+except:
+    print ("Invalid number proxy")
+    raise SystemExit
+
+def ping_url(number):
+
+    os.system(number)
+f = open("port.txt", "r")
+PORT = f.read()
+thread_list = []
+commands = []
+for i in range(number_proxy):
+    PORT = int(str(PORT)) - 1
+    commands.append(str('proxybroker serve --host 103.75.184.246 --port '+str(PORT) + ' --types HTTP HTTPS --lvl High -c US MAX_CONN 5'))
+
+for url in commands:
+
+    t = threading.Thread(target=ping_url, args=(url,))
+    thread_list.append(t)
+
+for thread in thread_list:
+    thread.start()
+
+for thread in thread_list:
+    thread.join()
+
+print("Done")
+
+
+
+
